7098ddd20e9c405ce9f97d8d43272e96e3029fbe
harika311221 <92515877+harika311221@users.noreply.github.com>
2021-10-17 22:54:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,39 +8,40 @@ aio = Client(username_1, key_1)
 from telegram.ext import Updater, MessageHandler, Filters
 
 def demo1(bot,update):
+  chat_id=bot.message.chat_id
+  bot.message.reply_text('Turning on the Light')
   aio.send('light',1)
-  chat_id = bot.message.chat_id
-  bot.message.reply_text('Light turned on')
 
 def demo2(bot,update):
+  chat_id=bot.message.chat_id
+  bot.message.reply_text('Turning off the Light')
   aio.send('light',0)
-  chat_id = bot.message.chat_id
-  bot.message.reply_text('Light turned off') 
 
 def demo3(bot,update):
+  chat_id=bot.message.chat_id
+  bot.message.reply_text('Turning on the Fan')
   aio.send('fan',1)
-  chat_id = bot.message.chat_id
-  bot.message.reply_text('Fan turned on')
 
 def demo4(bot,update):
+  chat_id=bot.message.chat_id
+  bot.message.reply_text('Turning off the Fan')
   aio.send('fan',0)
-  chat_id = bot.message.chat_id
-  bot.message.reply_text('Fan turned off')
 
 def main(bot,update):
   a= bot.message.text.lower()
-  if a =="turn on light":
+  if a=='lights on' or a==' turn on lights' or a==' turn lights on' or a==' turn on light' or a=='on light' or a=='light on' or a=='on lights' :
     demo1(bot,update)
-  elif a =="turn off light":
+  elif a=='lights off' or a==' turn off lights' or a==' turn lights off' or a==' turn off light' or a=='off light' or a=='light off' or a=='off lights' :
     demo2(bot,update)
-  elif a =="turn on fan":
+  elif a=='fans on' or a==' turn on fans' or a==' turn fans on' or a==' turn on fan' or a=='on fan' or a=='fan on ' or a=='on fans' :
     demo3(bot,update)
-  elif a =="turn off fan":
+  elif a=='fans off' or a==' turn off fans' or a=='turn fans off' or a==' turn fan off ' or a=='off fan' or a=='fan off'  or a=='off fans' :
     demo4(bot,update)
 
-bot_token = '1982159235:AAH7rqKN9Uw-kQ9yE_cFSxnZsDga05cs9Pk'
+
+bot_token ='2090379549:AAGnS3yTzjEIVFOB1Jk0f5Ej4OR8nCq25G8'
 u = Updater(bot_token,use_context=True)
-dp = u.dispatcher
+dp=u.dispatcher
 dp.add_handler(MessageHandler(Filters.text,main))
 u.start_polling()
 u.idle()
