04a22b12b2629283ec9fb84ea5b3da2fecd0ab3d
harika311221 <92515877+harika311221@users.noreply.github.com>
2021-10-18 20:18:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,50 +1,43 @@
 import os
 #pip install adafruit-io
 from Adafruit_IO import Client
-username = os.getenv('username')
-key = os.getenv('key')
-token=os.getenv('token')
-aio = Client(username,key)
-#pip install python-telegram-bot==13.0
-
+aio = Client('harika_31','aio_LNcj32v7bVrqlJeb4n5udx4aXF9H')
 from telegram.ext import Updater, MessageHandler, Filters
 
 def demo1(bot,update):
-  chat_id=bot.message.chat_id
-  bot.message.reply_text('Turning on the Light')
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('Light turned on')
   aio.send('light',1)
 
 def demo2(bot,update):
-  chat_id=bot.message.chat_id
-  bot.message.reply_text('Turning off the Light')
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('Light turned off') 
   aio.send('light',0)
 
 def demo3(bot,update):
-  chat_id=bot.message.chat_id
-  bot.message.reply_text('Turning on the Fan')
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('Fan turned on')
   aio.send('fan',1)
 
 def demo4(bot,update):
-  chat_id=bot.message.chat_id
-  bot.message.reply_text('Turning off the Fan')
+  chat_id = bot.message.chat_id
+  bot.message.reply_text('Fan turned off')
   aio.send('fan',0)
 
 def main(bot,update):
   a= bot.message.text.lower()
-  if a=='lights on' or a==' turn on lights' or a==' turn lights on' or a==' turn on light' or a=='on light' or a=='light on' or a=='on lights' :
+  if a =="turn on light":
     demo1(bot,update)
-  elif a=='lights off' or a==' turn off lights' or a==' turn lights off' or a==' turn off light' or a=='off light' or a=='light off' or a=='off lights' :
+  elif a =="turn off light":
     demo2(bot,update)
-  elif a=='lights off' or a==' turn off lights' or a==' turn lights off' or a==' turn off light' or a=='off light' or a=='light off' or a=='on fans' :
+  elif a =="turn on fan":
     demo3(bot,update)
-  elif a=='fans off' or a==' turn off fans' or a=='turn fans off' or a==' turn fan off ' or a=='off fan' or a=='fan off'  or a=='off fans' :
+  elif a =="turn off fan":
     demo4(bot,update)
-
 
 bot_token = token
 u = Updater(bot_token,use_context=True)
-dp=u.dispatcher
+dp = u.dispatcher
 dp.add_handler(MessageHandler(Filters.text,main))
 u.start_polling()
 u.idle()
-
